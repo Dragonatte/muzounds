b0311b375800adb36b06ba97b4b6285da003f18a
Dragonatte <rodrigomurillo666@hotmail.com>
2025-01-12 00:18:36 +0100

    next

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import Aside from "@/components/Aside";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import React from "react";
 
 export default function RootLayout({
   children,
